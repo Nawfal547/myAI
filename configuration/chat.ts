a90cd4f87840266e9c99d28a699828d158466568
Nawfal547 <92063236+Nawfal547@users.noreply.github.com>
2025-03-16 05:34:38 -0400

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,7 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, here to help you with everything and anything related
-to Ekkovision. Please let me know what questions you have.`;
+export const INITIAL_MESSAGE: string = `Hey there! I'm ${AI_NAME}, your go-to for all things Ekkovision. Whether it's product info, 
+company info, or recommendations, I’m here to help—just ask!`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
