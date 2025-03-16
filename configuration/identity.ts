eb070ee1ce9382ff4e5683ce7285da0f381ecd09
Nawfal547 <92063236+Nawfal547@users.noreply.github.com>
2025-03-16 05:11:29 -0400

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Nawfal Mohamed`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `The EKKO Bot`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
