d2f5987b6c2077638d3b5178fd0a2f9c4ab23fb0
Nawfal547 <92063236+Nawfal547@users.noreply.github.com>
2025-03-17 16:07:31 -0400

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,11 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `THE EKKO BOT`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `What would you like help with today?`;
 export const FOOTER_MESSAGE: string = `Customized AI by Nawfal Mohamed`;
 export const CLEAR_BUTTON_TEXT: string = `START NEW CHAT`;
 export const PAGE_TITLE: string = `EKKO BOT`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant 
+made specifically for the Ekkovision brand.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
